b3fbb5d8ff48879400b47f8f30cf6689f83c308d
Bluenarysun <tee.emilond@gmail.com>
2022-09-23 02:31:32 +0700

    rename api

--- src/pages/api/banner.js
+++ src/pages/api/banner.js
@@ -7,18 +7,11 @@ export default async function handler(req, res) {
     //Read the json data file data.json
     const fileContents = await fs.readFile(jsonDirectory + '/banner.json', 'utf8');
 
-    const jsonCategories = JSON.parse(fileContents);
+    const jsonBanner = JSON.parse(fileContents);
 
     // User with id exists
-    res.status(200).json(jsonCategories);
+    res.status(200).json(jsonBanner);
   } catch (error) {
     res.status(500).json({ error: 'Error reading data' });
   }
-
-  // //Find the absolute path of the json directory
-  // const jsonDirectory = path.join(process.cwd(), 'mock-data');
-  // //Read the json data file data.json
-  // const fileContents = await fs.readFile(jsonDirectory + '/banner.json', 'utf8');
-  // //Return the content of the data file in json format
-  // res.status(200).json(fileContents);
 }
