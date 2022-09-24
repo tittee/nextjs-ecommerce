5b6d9425661de1da97dc7bcd2fb225e4750a47e1
Bluenarysun <tee.emilond@gmail.com>
2022-09-24 23:58:14 +0700

    Seller page

--- src/pages/seller.tsx
+++ src/pages/seller.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { useForm } from 'react-hook-form';
 // import Service from 'lib/service';
 import { Heading2 } from '@common/Heading';
+import ButtonCommon from '@common/Button';
 
 interface HomeProps {
   banner: object;
@@ -17,35 +18,82 @@ const Seller: NextPage = ({ banner, products }: HomeProps) => {
 
   return (
     <Layout title="หน้าแรก SWOPMART">
-      <div className="container mt-10 mb-10 mx-auto bg-white">
-        <div className="p-4 md:p-6">
-          <Heading2>สินค้าและรายละเอียด</Heading2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="container mt-10 mx-auto">
+          <div className="p-4 md:p-6 bg-white rounded-lg">
+            <Heading2>สินค้าและรายละเอียด</Heading2>
 
-          <div className="w-full">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="flex">
-                <label>ชื่อสินค้า*</label>
-                <input {...register('firstName', { required: true, maxLength: 20 })} />
+            <div className="w-full mt-6">
+              <div className="flex flex-wrap">
+                <label className="flex-none w-full">ชื่อสินค้า*</label>
+                <input
+                  type="text"
+                  placeholder="ระบุชื่อสินค้า"
+                  {...register('firstName', {
+                    required: true,
+                    maxLength: 20,
+                  })}
+                />
               </div>
-              <div className="flex">
+              <div className="flex flex-wrap gap-6">
                 <div className="flex-1">
                   <label>ราคา</label>
-                  <input {...register('price', { pattern: /^[A-Za-z]+$/i })} />
+                  <input
+                    type="text"
+                    placeholder="ระบุราคา"
+                    {...register('price', { pattern: /^[A-Za-z]+$/i })}
+                  />
                 </div>
                 <div className="flex-1">
                   <label>เลือกสภาพสินค้า*</label>
                   <select {...register('tag')}>
+                    <option value="">ระบุสภาพสินค้า</option>
                     <option value="female">female</option>
                     <option value="male">male</option>
                     <option value="other">other</option>
                   </select>
                 </div>
               </div>
-              <input type="submit" />
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+        <div className="container mt-6 mx-auto">
+          <div className="flex flex-wrap gap-2 w-full justify-center md:justify-end">
+            <div className="flex-none">
+              <ButtonCommon
+                buttonSize="normal"
+                buttonLabel="ยกเลิก"
+                buttonType="outline"
+                fontWeight="bold"
+                style={{
+                  textColor: 'button',
+                  backgroundColor: '',
+                }}
+                hoverStyle={{
+                  textColor: 'button',
+                  backgroundColor: 'white',
+                }}
+              />
+            </div>
+            <div className="flex-none">
+              <ButtonCommon
+                buttonSize="normal"
+                buttonLabel="บันทึก"
+                buttonType="primary"
+                fontWeight="bold"
+                style={{
+                  textColor: 'white',
+                  backgroundColor: 'button',
+                }}
+                hoverStyle={{
+                  textColor: 'orange',
+                  backgroundColor: 'button',
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 };
